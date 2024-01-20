13f09594dff96fae2d34fee4d511937138f6d3fc
heneli627627 <102131609+hen627@users.noreply.github.com>
2024-01-20 19:12:11 +0100

    Update scriptsNfunctions.test.ts

--- tests/scriptsNfunctions.test.ts
+++ tests/scriptsNfunctions.test.ts
@@ -1,6 +1,4 @@
-// in this I will try to record a test script, 
-//go through reports, 
-//screenshots $ videos on failure, 
-//execution on failure, 
-//Auto-wait mechanism.
-
+// interaction with inputs
+// interaction with buttons
+// interaction with checkbox
+// assertions
